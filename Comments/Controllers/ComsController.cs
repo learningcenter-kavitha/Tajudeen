5d3fe746a836470ab2d13cec385a630efa1e629f
learningcenter-kavitha <learningcenter2020@outlook.com>
2019-02-20 17:46:17 +0530

    first commit

---
+++ Comments/Controllers/ComsController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Comments.Models;
+
+namespace Comments.Controllers
+{
+    public class ComsController : Controller
+    {
+        private temp1Entities db = new temp1Entities();
+
+        // GET: Coms
+        public ActionResult Index()
+        {
+            return View(db.Coms.ToList());
+        }
+
+        // GET: Coms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Com com = db.Coms.Find(id);
+            if (com == null)
+            {
+                return HttpNotFound();
+            }
+            return View(com);
+        }
+
+        // GET: Coms/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Coms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Name,Password,Email,Age")] Com com)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Coms.Add(com);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(com);
+        }
+
+        // GET: Coms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Com com = db.Coms.Find(id);
+            if (com == null)
+            {
+                return HttpNotFound();
+            }
+            return View(com);
+        }
+
+        // POST: Coms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Name,Password,Email,Age")] Com com)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(com).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(com);
+        }
+
+        // GET: Coms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Com com = db.Coms.Find(id);
+            if (com == null)
+            {
+                return HttpNotFound();
+            }
+            return View(com);
+        }
+
+        // POST: Coms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Com com = db.Coms.Find(id);
+            db.Coms.Remove(com);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Comments()
+        {
+            return View();
+        }
+        public ActionResult login(Com reg, string Email, string Password)
+        {
+
+            reg.Email = Email;
+            reg.Password = Password;
+            var Register = db.Coms.ToList();
+            var user = Register.SingleOrDefault(usr => usr.Email == reg.Email && usr.Password == reg.Password);
+            if (user != null)
+            {
+                Session["Email"] = user.Email.ToString();
+                Session["Password"] = user.Password.ToString();
+                return RedirectToAction("Comments", "Coms");
+            }
+            return View();
+        }
+    }
+}
---
+++ Comments/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Comments")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Comments")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d2765ebc-eb03-45c5-9406-7972939f7770")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
